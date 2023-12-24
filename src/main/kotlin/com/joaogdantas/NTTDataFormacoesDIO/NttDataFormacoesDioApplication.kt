a2850d2db5ac893e3da5aaa95da7bf4789fb9288
João Dantas <joaogdantasdev@hotmail.com>
2023-12-24 14:49:54 -0300

    config: add project basic structure

---
+++ src/main/kotlin/com/joaogdantas/NTTDataFormacoesDIO/NttDataFormacoesDioApplication.kt
@@ -0,0 +1,12 @@
+package com.joaogdantas.NTTDataFormacoesDIO
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NttDataFormacoesDioApplication
+
+	fun main(args: Array<String>) {
+		runApplication<NttDataFormacoesDioApplication>(*args)
+	}
+
